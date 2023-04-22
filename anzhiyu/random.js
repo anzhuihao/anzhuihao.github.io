068faa926a3e53faeca2a52227b7f6d4ad71594d
anzhuihao <mzh20080208@icloud.com>
2023-04-23 00:57:28 +0800

    Site updated: 2023-04-23 00:57:28

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/0.html","posts/bc51e6d.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/bc51e6d.html","posts/0.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
