0dad5b033eb90560aac62ff28429b808875ae6c2
anzhuihao <mzh20080208@icloud.com>
2023-05-01 19:01:33 +0800

    Site updated: 2023-05-01 19:01:33

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/4a17b156.html","posts/ab65d9e6.html","posts/bc51e6d.html","posts/e96bb967.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/4a17b156.html","posts/ab65d9e6.html","posts/e96bb967.html","posts/bc51e6d.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
