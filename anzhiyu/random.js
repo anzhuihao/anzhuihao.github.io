87c180daadc7ede1bdb856bb98de17652c6fad1d
馬志豪 <mazhihao@192.168.10.102>
2023-05-04 00:40:42 +0800

    Site updated: 2023-05-04 00:40:42

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/4a17b156.html","posts/ab65d9e6.html","posts/fb374582.html","posts/e96bb967.html","posts/bc51e6d.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/4a17b156.html","posts/ab65d9e6.html","posts/e96bb967.html","posts/fb374582.html","posts/bc51e6d.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
