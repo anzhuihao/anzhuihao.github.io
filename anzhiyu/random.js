498c43ee5a994836f4fd05194cc4e7a70f259d78
anzhuihao <mzh20080208@icloud.com>
2023-05-07 22:47:53 +0800

    Site updated: 2023-05-07 22:47:53

--- anzhiyu/random.js
+++
@@ -1,1 +0,0 @@
-var posts=["posts/ab65d9e6.html","posts/e96bb967.html","posts/bc51e6d.html","posts/fb374582.html","posts/4a17b156.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
